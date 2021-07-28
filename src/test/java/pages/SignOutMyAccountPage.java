ef9430c379ab334b423f32c6beba1b6762bbf4d9
jessicasbatista <84240812+jessicasbatista@users.noreply.github.com>
2021-07-28 20:52:46 -0300

    Update SignOutMyAccountPage.java

--- src/test/java/pages/SignOutMyAccountPage.java
+++ src/test/java/pages/SignOutMyAccountPage.java
@@ -6,22 +6,13 @@ import org.openqa.selenium.WebDriver;
 public class SignOutMyAccountPage {
 	
 	protected WebDriver driver;
-//	By Text = By.cssSelector("ul.myaccount-link-list li a span");
 	By clickOnSignOut = By.className("logout");
 
 	public SignOutMyAccountPage(WebDriver driver) {
 		this.driver = driver;
 	}
-
-//	public void MyAccount() {
-//		String getmyaccounttext = driver.findElement(Text).getText();
-//		assertEquals("ORDER HISTORY AND DETAILS", getmyaccounttext);
-//	}
 	
 	public void clickOnSignOut() {
 		driver.findElement(clickOnSignOut).click();
-		
 	}
-	
-
 }
